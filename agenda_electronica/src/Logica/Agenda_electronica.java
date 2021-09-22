a6c1648b484274f5aa28697c15d06bf57e5c88da
andrea <87381785+andrea813@users.noreply.github.com>
2021-09-22 14:56:21 -0300

    Add files via upload

---
+++ agenda_electronica/src/Logica/Agenda_electronica.java
@@ -0,0 +1,18 @@
+
+package Logica;
+
+import IGU.Principal;
+
+
+public class Agenda_electronica {
+
+
+    public static void main(String[] args) {
+       
+        Principal pantalla = new Principal ();
+        pantalla.setVisible(true);
+        pantalla.setLocationRelativeTo(null);
+        
+    }
+    
+}
